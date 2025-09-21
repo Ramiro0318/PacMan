8564ea1dfbee6a38dbe1fee99836eb72d5d9edad
Ramiro Valverde <ramiro.val03@gmail.com>
2025-09-21 16:41:23 -0600

    Correcion Pausa
    Modificacion a ControlPausa para que ahora no se muevan ni Pacman ni los fantasmas durante la pausa, tambien se detienen sus animators.

--- Assets/Scrips/ControlPausa.cs
+++ Assets/Scrips/ControlPausa.cs
@@ -1,25 +1,36 @@
 using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class ControlPausa : MonoBehaviour
 {
     public GameObject CanvasPausa;
-    private bool pausa = false;
+    public GameObject btnDefault;
+    
     Keyboard keyboard = Keyboard.current;
+    Animator anim;
+
+    private bool pausa = false;
 
     private void Start()
     {
         if (CanvasPausa != null)
             CanvasPausa.SetActive(false);
+        foreach (Animator anim in FindObjectsByType<Animator>(FindObjectsSortMode.None))
+        {
+                anim.enabled = true;
+        }
+        Time.timeScale = 1f;
     }
 
     private void Update()
     {
-        CambuarPausa();
+        CambiarPausa();
     }
 
-    public void CambuarPausa()
+    public void CambiarPausa()
     {
         if (keyboard == null) return;
         if (keyboard.escapeKey.wasPressedThisFrame)
@@ -27,11 +38,22 @@ public class ControlPausa : MonoBehaviour
         {
             if (pausa)
             {
-                DesactivarPausa();
+                DesactivarPausa(); //Desactivar
             }
             else
             {   //Activar Pausa
                 CanvasPausa.SetActive(true);
+                EventSystem.current.SetSelectedGameObject(btnDefault);
+
+                foreach (Animator anim in FindObjectsByType<Animator>(FindObjectsSortMode.None))
+                {
+                    //if (anim.CompareTag("Personaje"))
+                    {
+                        anim.enabled = false;
+                    }
+                }
+                Time.timeScale = 0f;
+                
                 pausa = true;
                 Debug.Log($"Pausa = {pausa}");
             }
@@ -41,6 +63,15 @@ public class ControlPausa : MonoBehaviour
     public void DesactivarPausa()
     {
         CanvasPausa.SetActive(false);
+        Time.timeScale = 1f;
+        foreach (Animator anim in FindObjectsByType<Animator>(FindObjectsSortMode.None))
+        {
+            //if (anim.CompareTag("Personaje"))
+            {
+                anim.enabled = true;
+            }
+        }
+
         pausa = false;
         Debug.Log($"Pausa = {pausa}");
     }
