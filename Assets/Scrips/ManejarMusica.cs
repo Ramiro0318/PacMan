0f36ba441819008929d3bd07b76c03f0283b9609
Ramiro Valverde <ramiro.val03@gmail.com>
2025-09-21 18:10:16 -0600

    Funcion muteo e imagenes en botones
    Se creo una funcion para mutear la musica desde el MuscManager. Se agregaron imagenes que funcionan con una flag para muteo de musica y de sonido

--- Assets/Scrips/ManejarMusica.cs
+++ Assets/Scrips/ManejarMusica.cs
@@ -1,14 +1,28 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class ManejarMusica: MonoBehaviour
 {
     public AudioSource audioSource;
     public WaitForSeconds seconds = new WaitForSeconds(0.1f);
-    float loopStart = 0f;
-    float loopEnd = 4.94f;
-    int loops = 0;
+    private float loopStart = 0f;
+    private float loopEnd = 4.94f;
+    private int loops = 0;
+    private bool mute = false;
+    
+    //Musica
+    public Image iconImage;
+    public Sprite muteIcon;
+    public Sprite unmuteIcon;
+
+    //Sonido
+    public Image SonidoIconImage;
+    public Sprite SonidoMuteIcon;
+    public Sprite SonidoUnmuteIcon;
+    private bool sonidoMute = false;
+
 
     void Start()
     {
@@ -26,5 +40,23 @@ public class ManejarMusica: MonoBehaviour
             loops ++;
         }
     }
+
+
+
+
+    public void SilenciarMusica()
+    {
+        mute = !mute;
+        audioSource.mute = mute;
+        iconImage.sprite = mute ? muteIcon : unmuteIcon;
+    }
+
+    public void SilenciarSonido()
+    {
+        sonidoMute = !sonidoMute;
+        SonidoIconImage.sprite = sonidoMute ? SonidoMuteIcon : SonidoUnmuteIcon;
+    }
+
+
 
 }
