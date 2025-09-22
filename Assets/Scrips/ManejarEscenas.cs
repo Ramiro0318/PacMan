1cbb919face0d41fd46bab97a3dd79124a85b9bd
Ramiro Valverde <ramiro.val03@gmail.com>
2025-09-22 16:08:18 -0600

    Reinicio y Salir
    Manejo de escenas de reinicio y salir, tambien icnono y configuracion de build

--- Assets/Scrips/ManejarEscenas.cs
+++ Assets/Scrips/ManejarEscenas.cs
@@ -3,12 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class ManejarEscenas : MonoBehaviour
 {
+    GameObject CanvasPausa; //= GameObject.Find("CanvasPausa");
+    GameObject CanvasGameOver;// = GameObject.Find("CanvasGameOver");
+
     //public string EscenaSiguiente; //Asignar el nonmbre de escena
 
     public void IrAEscena(string escena)
     {
         //SceneManager.LoadScene(EscenaSiguiente); //esto selecciona la escena desde SceneManager
         SceneManager.LoadScene(escena);
+        //if (CanvasPausa.activeInHierarchy)
+        {
+        }
+        CanvasPausa = GameObject.Find("CanvasPausa");
+        CanvasGameOver = GameObject.Find("CanvasGameOver");
+        CanvasGameOver.SetActive(false);
+        CanvasPausa.SetActive(false);
     }
         
     public void SalirJuego()
